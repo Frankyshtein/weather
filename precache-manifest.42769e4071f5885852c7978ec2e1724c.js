669cc77205aff26c970a3a2c898cf0eb88ef6505
Frankyshtein <amid0maru53@gmail.com>
2018-12-02 16:00:05 +0300

    Updates

---
+++ precache-manifest.42769e4071f5885852c7978ec2e1724c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "d0fc91304cdf0e26c149",
+    "url": "/weather/static/js/main.d0fc9130.chunk.js"
+  },
+  {
+    "revision": "1b532d5175851ba2ef81",
+    "url": "/weather/static/js/1.1b532d51.chunk.js"
+  },
+  {
+    "revision": "d0fc91304cdf0e26c149",
+    "url": "/weather/static/css/main.c0c4e563.chunk.css"
+  },
+  {
+    "revision": "1b532d5175851ba2ef81",
+    "url": "/weather/static/css/1.3a96613d.chunk.css"
+  },
+  {
+    "revision": "75bf5fd9c90ce74b335e74bb91ecaee0",
+    "url": "/weather/index.html"
+  }
+];
